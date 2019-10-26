4863a41380c165da69997657e4990aab3295e403
Chase Norman <c_@berkeley.edu>
2019-10-25 23:43:59 -0700

    initial commit

---
+++ thymesia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  thymesia
+//
+//  Created by Chase Norman on 10/25/19.
+//  Copyright © 2019 Chase Norman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
