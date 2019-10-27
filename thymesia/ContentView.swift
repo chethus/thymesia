4bc23c19d1fbf8c572c05187d880402b4bf364fc
Chase Norman <c_@berkeley.edu>
2019-10-27 01:22:15 -0700

    removed Contact

--- thymesia/ContentView.swift
+++ thymesia/ContentView.swift
@@ -12,11 +12,15 @@ import ContactsUI
 
 let keys = [CNContactInstantMessageAddressesKey,CNContactGivenNameKey,CNContactTypeKey,CNContactDatesKey,CNContactBirthdayKey,CNContactNicknameKey,CNContactRelationsKey,CNContactIdentifierKey,CNContactJobTitleKey,CNContactImageDataKey,CNContactFamilyNameKey,CNContactMiddleNameKey,CNContactNamePrefixKey,CNContactNameSuffixKey,CNContactPhoneNumbersKey,CNContactUrlAddressesKey,CNContactDepartmentNameKey,CNContactEmailAddressesKey,CNContactSocialProfilesKey,CNContactPostalAddressesKey,CNContactOrganizationNameKey,CNContactPhoneticGivenNameKey,CNContactImageDataAvailableKey,CNContactPhoneticFamilyNameKey,CNContactPhoneticMiddleNameKey,CNContactPreviousFamilyNameKey,CNContactThumbnailImageDataKey,CNContactNonGregorianBirthdayKey,CNContactPhoneticOrganizationNameKey] as [CNKeyDescriptor]
 
-let emptyContact = Contact(CNMutableContact())
+let identifier = [CNContactIdentifierKey] as [CNKeyDescriptor];
+
+let emptyEntry = Entry("","")
 
 
 // Hashable tuple of length 2.
-struct Entry : Hashable, CustomStringConvertible {
+struct Entry : Hashable, CustomStringConvertible, Identifiable {
+    var id = UUID()
+    
     static func == (lhs: Entry, rhs: Entry) -> Bool {
         return lhs.key == rhs.key && lhs.value == rhs.value
     }
@@ -26,8 +30,8 @@ struct Entry : Hashable, CustomStringConvertible {
         value = u
     }
     
-    let key : String
-    let value : String
+    var key : String
+    var value : String
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(key)
@@ -37,97 +41,22 @@ struct Entry : Hashable, CustomStringConvertible {
     public var description: String { return "\(key):\(value)" }
 }
 
-// Contact interface
-struct Contact : Hashable{
-    static func == (lhs: Contact, rhs: Contact) -> Bool {
-        return lhs.contact == rhs.contact
-    }
-    
-    var contact: CNMutableContact
-    var data: [Entry] = []
-    var name: String {
-        get {
-            return CNContactFormatter.string(from: contact, style: .fullName) ?? "Unnamed"
-        }
-    }
-    var allData: [Entry] = []
-    
-    init() {
-        self.contact = CNMutableContact()
-        let saveRequest = CNSaveRequest()
-        saveRequest.add(contact, toContainerWithIdentifier: nil)
-        try! CNContactStore().execute(saveRequest)
-        self.data = []
-        allData = []
-    }
-    
-    init(_ contact: CNMutableContact) {
-        self.contact = contact
-        fetchData();
-    }
-    
-    func update() {
-        contact.instantMessageAddresses = []
-        for entry in data {
-            contact.instantMessageAddresses.append(CNLabeledValue(label: entry.key, value: CNInstantMessageAddress(username: entry.value, service: entry.key)))
-        }
-        let saveRequest = CNSaveRequest()
-        saveRequest.update(contact)
-        try! CNContactStore().execute(saveRequest)
-    }
-    
-    func delete() {
-        let saveRequest = CNSaveRequest()
-        saveRequest.delete(contact)
-        try! CNContactStore().execute(saveRequest)
-    }
-    
-    mutating func replace(_ e1: Entry, _ e2: Entry) {
-        for i in 0..<data.count {
-            if (data[i] == e1) {
-                data[i] = e2
-                break;
-            }
-        }
-        update();
-    }
-    
-    mutating func fetch() {
-        contact = try! CNContactStore().unifiedContact(withIdentifier: contact.identifier, keysToFetch: keys).mutableCopy() as! CNMutableContact
-        fetchData();
-    }
-    
-    mutating func fetchData() {
-        self.data = []
-        self.allData = []
-        
-        
-        
-        for address in contact.instantMessageAddresses {
-            let e = Entry(address.value.service, address.value.username)
-            self.data.append(e)
-            self.allData.append(e)
-        }
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(contact)
-    }
-}
-
 // Main view
 struct ContentView: View {
     
-    @State var contacts: [Contact] = [];
-    @State var edits: [Contact] = []
+    @State var contacts: [String] = [];
+    @State var edits: [String] = []
     @State var searchText: String = "";
     @State var currentPage = 0
     
     
     func delete(at offsets: IndexSet) {
         for i in offsets {
+            let x = (try! CNContactStore().unifiedContact(withIdentifier: contacts[i], keysToFetch: keys).mutableCopy() as! CNMutableContact)
+            let save = CNSaveRequest()
+            save.delete(x);
+            try! CNContactStore().execute(save);
     
-            contacts[i].delete()
             contacts.remove(at: i)
         }
     }
@@ -142,32 +71,36 @@ struct ContentView: View {
                             NavigationLink(destination: ContactDetail(contact).navigationBarItems(trailing: Button("Edit"){
                                 self.edits.append(contact)
                             })) {
-                                Text(contact.name)
+                                Text(contact)
                             }
                         }.onDelete(perform: delete)
                     }
                 }.navigationBarItems(trailing:
                     Button(action: {
-                        let contact = Contact()
-                        self.edits.append(contact)
-                        self.contacts.insert(contact, at: 0)
+                        let c = CNMutableContact();
+                        let save = CNSaveRequest();
+                        save.add(c, toContainerWithIdentifier: nil)
+                        try! CNContactStore().execute(save)
+                        
+                        self.edits.append(c.identifier)
+                        self.contacts.insert(c.identifier, at: 0)
                     }) {
                         Image(systemName: "plus").imageScale(.large).padding()
                 }).navigationBarTitle(Text("Search"))
             }.tabItem{
                 Image(systemName: "magnifyingglass")
             }
-            ForEach(0..<self.edits.count, id: \.self) { edit in
+            ForEach(edits, id: \.self) { edit in
                 NavigationView {
-                    List {
-                        ForEach(0..<self.edits[edit].data.count, id: \.self) {entry in
-                            TextField("", text: $edits[edit].data[entry].key)
-                        }
-                    }.navigationBarTitle(Text(self.edits[edit].name),displayMode: .inline).navigationBarItems(trailing:
+                    ContactField(edit).navigationBarTitle(Text(edit),displayMode: .inline).navigationBarItems(trailing:
                         Button(action: {
-                            let contact = Contact()
-                            self.edits.append(contact)
-                            self.contacts.insert(contact, at: 0)
+                            let c = CNMutableContact();
+                            let save = CNSaveRequest();
+                            save.add(c, toContainerWithIdentifier: nil)
+                            try! CNContactStore().execute(save)
+                            
+                            self.edits.append(c.identifier)
+                            self.contacts.insert(c.identifier, at: 0)
                         }) {
                             Image(systemName: "plus").imageScale(.large).padding()
                     })
@@ -176,62 +109,70 @@ struct ContentView: View {
                 }
             }
         }.onAppear {
-            let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
-            try! CNContactStore().enumerateContacts(with: request) {
-                (contact, stop) in
-                self.contacts.insert(Contact(contact.mutableCopy() as! CNMutableContact), at: 0)
-            }
+            self.refreshContacts()
         }
+    }
+    
+    func refreshContacts() {
+        contacts = []
+        let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+        try! CNContactStore().enumerateContacts(with: request) {
+            (contact, stop) in
+            self.contacts.insert(contact.identifier, at: 0)
+        }
+    }
+    
+    func randomString(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      return String((0..<length).map{ _ in letters.randomElement()! })
     }
 }
 
-/*struct ContactField : View {
-    let viewModel: Contact
-    @State private var entries: [Entry] = []
-    var didUpdateEntries: [Entry] -> ()
+struct ContactField : View {
+    let viewModel: String
+    @State private var entries: [Entry] = Array<Entry>(repeating: emptyEntry, count: 50)
+    @State private var len = 0;
     
-    init(_ viewModel: Contact, _ didUpdateText: @escaping (String, String)->()) {
+    init(_ viewModel: String) {
         self.viewModel = viewModel
-        self.didUpdateText = didUpdateText
     }
 
     var body: some View {
-        List {
-            ForEach (entries, id: \.self) {entry in
-                EntryField(entry) {k,v in
-                    
+        VStack {
+            ForEach (entries.indices) {index in
+                if (index < self.len) {
+                    HStack {
+                        TextField("", text: self.$entries[index].key, onCommit: {
+                            self.update();
+                        })
+                        TextField("", text: self.$entries[index].value, onCommit: {
+                            self.update();
+                        })
+                    }
                 }
             }
         }.onAppear() {
-            self.entries = self.viewModel.data
+            let x = try! CNContactStore().unifiedContact(withIdentifier: self.viewModel, keysToFetch: keys)
+            let attr = x.instantMessageAddresses;
+            
+            for i in 0..<attr.count {
+                self.entries[i] = Entry(attr[i].value.service, attr[i].value.username)
+            }
+            self.len = attr.count
         }
     }
-}*/
-
-struct EntryField : View {
-    let viewModel: Entry
-    @State private var key: String = ""
-    @State private var value: String = ""
-    var didUpdateText: (String, String) -> ()
     
-    init(_ viewModel: Entry, _ didUpdateText: @escaping (String, String)->()) {
-        self.viewModel = viewModel
-        self.didUpdateText = didUpdateText
-    }
-
-    var body: some View {
-        HStack {
-            TextField(viewModel.key, text: $key, onCommit: {
-                self.didUpdateText(self.key, self.value)
-            }).onAppear {
-                self.key = self.viewModel.key
-            }
-            TextField(viewModel.value, text: $value, onCommit: {
-                self.didUpdateText(self.key, self.value)
-            }).onAppear {
-                self.value = self.viewModel.value
-            }
+    func update() {
+        let x = (try! CNContactStore().unifiedContact(withIdentifier: self.viewModel, keysToFetch: keys).mutableCopy() as! CNMutableContact)
+        x.instantMessageAddresses = []
+        
+        for i in self.entries {
+            x.instantMessageAddresses.append(CNLabeledValue<CNInstantMessageAddress>(label: i.key, value: CNInstantMessageAddress(username: i.value, service: i.key)))
         }
+        
+        let save = CNSaveRequest()
+        save.update(x);
+        try! CNContactStore().execute(save);
     }
 }
 
@@ -244,22 +185,35 @@ struct ContentView_Previews: PreviewProvider {
 
 // Shows the detail of a contact
 struct ContactDetail : View {
-    var contact: Contact
+    let viewModel: String
+    @State private var entries: [Entry] = []
     
-    
-    init(_ contact: Contact) {
-        self.contact = contact;
+    init(_ viewModel: String) {
+        self.viewModel = viewModel
+        self.refresh();
+        print(entries);
     }
-    
+
     var body: some View {
-        List {
-            ForEach (contact.data, id: \.self) {entry in
+        VStack {
+            ForEach (entries) {entry in
                 HStack {
                     Text(entry.key)
                     Text(entry.value)
                 }
             }
-        }.navigationBarTitle(Text(contact.name),displayMode: .inline)
+        }.onAppear {
+            self.refresh();
+        }
+    }
+    
+    func refresh() {
+        let x = try! CNContactStore().unifiedContact(withIdentifier: self.viewModel, keysToFetch: keys)
+        let attr = x.instantMessageAddresses;
+        
+        for i in attr {
+            self.entries.append(Entry(i.value.service,i.value.username))
+        }
     }
 }
 
